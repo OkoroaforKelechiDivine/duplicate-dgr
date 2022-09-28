b4e2bee519d0fa53b3e8137ad804898453f72e94
AMD <michojekunle1@gmail.com>
2022-09-28 19:54:19 +0100

    made changes

--- js/main.js
+++ js/main.js
@@ -1,10 +1,21 @@
 const toggleNavButton = document.querySelector('#toggle-menu');
 const miniMenu = document .querySelectorAll('.nav-link .dropdown li span');
+// const navLinks = document.querySelectorAll('.nav-link');
+
+// navLinks.forEach(link => {
+//     link.addEventListener('click', () => {
+//         link.classList.toggle('active');
+//     })
+// })
+
+console.log(window.innerWidth)
 
 miniMenu.forEach(drpdn => {
     drpdn.addEventListener('click', () => {
         console.log('clicked');
-        miniMenu.forEach(drpdn => drpdn.parentElement.classList.remove('active'))
+        if(window.innerWidth > 1400) {
+            miniMenu.forEach(drpdn => drpdn.parentElement.classList.remove('active'));
+        }
         drpdn.parentElement.classList.toggle('active');
     })
 })
