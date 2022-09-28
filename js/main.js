9568e758d444ba8e81d0defd78f644d66ba4b109
AMD <michojekunle1@gmail.com>
2022-09-29 00:07:07 +0100

    done with the hompage hero section

--- js/main.js
+++ js/main.js
@@ -1,12 +1,7 @@
 const toggleNavButton = document.querySelector('#toggle-menu');
 const miniMenu = document .querySelectorAll('.nav-link .dropdown li span');
-// const navLinks = document.querySelectorAll('.nav-link');
-
-// navLinks.forEach(link => {
-//     link.addEventListener('click', () => {
-//         link.classList.toggle('active');
-//     })
-// })
+const carouselButtonPrev = document.getElementById('carousel-button-prev');
+const carouselButtonNext = document.getElementById('carousel-button-next');
 
 console.log(window.innerWidth)
 
@@ -23,4 +18,42 @@ miniMenu.forEach(drpdn => {
 toggleNavButton.addEventListener('click', (e) => {
     e.target.classList.toggle('active');
     document.querySelector('.nav-links').classList.toggle('active');
-})
+});
+
+// Carousel 
+const heroBGs = [
+    './Assets/hero-bg.png',
+    './Assets/hero-bg-1.jpeg',
+    './Assets/hero-bg-2.jpeg'
+];
+
+let currentBg = 0;
+
+carouselButtonNext.addEventListener('click', () => {
+    if (currentBg+1 < heroBGs.length){
+        currentBg++;
+    } else {
+        currentBg = 0;
+    }
+    
+    document.querySelector('.hero-section').style.background = `linear-gradient(180deg, rgba(137, 0, 6, 0.46) 0%, rgba(0, 0, 0, 0.4) 85.5%), url(${heroBGs[currentBg]})`;
+    document.querySelector('.hero-section').style.backgroundRepeat = 'no-repeat';
+    document.querySelector('.hero-section').style.backgroundSize = `100% 100%`;
+    document.querySelector('.hero-section').style.animation = `animate-bg 2s ease-in-out ${currentBg}`;
+    // document.querySelector('.hero-section').style.animation = `animate-bg 2s ease-in-out 0`;
+});
+
+carouselButtonPrev.addEventListener('click', () => {
+    if (currentBg <= 2 && currentBg > 0){
+        currentBg--;
+    } else {
+        currentBg = 2;
+    }
+    
+    document.querySelector('.hero-section').style.background = `linear-gradient(180deg, rgba(137, 0, 6, 0.46) 0%, rgba(0, 0, 0, 0.4) 85.5%), url(${heroBGs[currentBg]})`;
+    document.querySelector('.hero-section').style.backgroundRepeat = 'no-repeat';
+    document.querySelector('.hero-section').style.backgroundSize = `100% 100%`;
+    document.querySelector('.hero-section').style.animation = `animate-bg 2s ease-in-out ${currentBg}`;
+    // document.querySelector('.hero-section').style.animation = `animate-bg 2s ease-in-out 0`;
+});
+
